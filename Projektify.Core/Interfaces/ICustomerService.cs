2768099ce440db954da8a5c14b22f30b8d682732
ReneSass00 <rene.sass00@web.de>
2025-10-23 21:52:04 +0200

    added crud operations for customers

--- Projektify.Core/Interfaces/ICustomerService.cs
+++ Projektify.Core/Interfaces/ICustomerService.cs
@@ -1,10 +1,12 @@
-﻿
-using Projektify.Core.Entities;
+﻿using Projektify.Core.Entities;
 
 namespace Projektify.Core.Interfaces;
 
 public interface ICustomerService
 {
     Task<List<Customer>> GetAllCustomersAsync();
+    Task<Customer?> GetCustomerByIdAsync(Guid id);
     Task CreateCustomerAsync(Customer customer);
+    Task UpdateCustomerAsync(Customer customer);
+    Task DeleteCustomerAsync(Guid id);
 }
--- Projektify.Infrastructure/Services/CustomerService.cs
+++ Projektify.Infrastructure/Services/CustomerService.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Projektify.Core.Entities;
 using Projektify.Core.Interfaces;
 
@@ -17,13 +16,35 @@ public class CustomerService : ICustomerService
     public async Task<List<Customer>> GetAllCustomersAsync()
     {
         return await _context.Customers
-            .Where(c => !c.IsArchived) 
+            .Where(c => !c.IsArchived)
+            .OrderBy(c => c.CompanyName)
             .ToListAsync();
+    }
+
+    public async Task<Customer?> GetCustomerByIdAsync(Guid id)
+    {
+        return await _context.Customers.FindAsync(id);
     }
 
     public async Task CreateCustomerAsync(Customer customer)
     {
         _context.Customers.Add(customer);
         await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateCustomerAsync(Customer customer)
+    {
+        _context.Entry(customer).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteCustomerAsync(Guid id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+        if (customer != null)
+        {
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+        }
     }
 }
